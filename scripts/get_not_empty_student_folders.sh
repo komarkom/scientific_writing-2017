652465445912139a2bd4f36a87de3d4fdd9b49e9
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2017-10-30 13:53:23 +0300

    added better output format

--- scripts/get_not_empty_student_folders.sh
+++ scripts/get_not_empty_student_folders.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+prefix="https://github.com/moevm/scientific_writing-2017/tree/master"
 for i in $(./scripts/get_student_dirs.sh)
 do 
   is_empty=$(ls $i| wc -l )
   if [ "$is_empty" != "0" ]
   then
-    echo "$(git log --pretty=format:"%ad %an" --date=iso-strict "$i" | grep -v 'Zaslavskiy' | sed -e 's/ [^ ]\+//g' | tail -1), $i"
-
+    echo "$(git log --pretty=format:"%ad %an" --date=iso-strict "$i" | grep -v 'Zaslavskiy' | sed -e 's/ [^ ]\+//g' | tail -1), $prefix/$i"
   fi
-done | sort -r
+done | sort -r | sed -e 's/^[^ ]\+, //g'
